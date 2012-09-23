807facae795a2522ba06ce1a69dd103c162de972
Thomas Brus <thomas.brus@me.com>
2012-09-23 23:30:13 +0200

    Small change to quicksort

--- exercises-2/exercise-11-quicksort.hs
+++ exercises-2/exercise-11-quicksort.hs
@@ -1,12 +1,8 @@
--- Opgave 11. Het quick sort algoritme verdeelt de lijst in elementen
--- die kleiner zijn dan het eerste element, gelijk zijn aan het eerste element,
--- en groter zijn dan het eerste element. Dit proces wordt herhaald voor de af-
--- zonderlijke lijsten. Schrijf de functie qsort.
-
 import FPPrac
 
 quicksort :: Ord a => [a] -> [a]
 quicksort []      = []
-quicksort (x:xs)  = (quicksort as) ++ [x] ++ (quicksort bs)
+quicksort (x:xs)  = (quicksort as) ++ x:bs ++ (quicksort cs)
                   where as = filter (< x) xs
-                        bs = filter (>= x) xs
+                        bs = filter (== x) xs
+                        cs = filter (> x) xs
--- exercises-2/exercise-9-insertion-sort.hs
+++ exercises-2/exercise-9-insertion-sort.hs
@@ -1,9 +1,3 @@
--- Opgave 9. Het algoritme insertion sort neemt bij het sorteren
--- van een lijst de lege lijst als uitgangspunt en voegt de
--- elementen van de te sorteren lijst een voor een op de juiste plaats toe.
--- Schrijf de functie isort (de Haskell module List kent al een functie
--- insert). Maak gebruik van een van de fold functies.
-
 import FPPrac
 import Data.List (insert)
  
