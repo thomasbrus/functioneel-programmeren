e5274d2a7fd190b8220e07747a87c7c93b505695
Thomas Brus <thomas.brus@me.com>
2012-09-26 17:46:13 +0200

    Fixed exercise 1.8

--- exercises-1/exercise-8-list-properties.hs
+++ exercises-1/exercise-8-list-properties.hs
@@ -1,34 +1,9 @@
--- – Schrijf een functie allEqual die nagaat
--- of alle elementen van een lijst gelijk zijn.
-
 import FPPrac
 
 allEqual :: (Eq a) => [a] -> Bool
-allEqual (x:[]) = True
-allEqual (x:xs) = (x == head xs) && allEqual xs
+allEqual [x] = True
+allEqual (x:y:xs) = x == y && allEqual (y:xs)
 
 isRR :: [Number] -> Bool
-isRR (x:[y,z]) = y - x == z - y
-isRR (x:xs) = (head xs) - x == (head (tail xs)) - (head xs) && isRR xs
-
-{-|
-  *Main> allEqual [2,2,2]
-  True
-  *Main> allEqual [2,1,2]
-  False
-  *Main> allEqual [2,1,1]
-  False
-  *Main> allEqual []
-  *** Exception: check.hs:(7,1)-(8,47): Non-exhaustive patterns in function allEqual
-  *Main> allEqual [1]
-  True
-
-  *Main> isRR [1,2,3,4]
-  True
-  *Main> isRR [1,2,3,5]
-  False
-  *Main> isRR [-1,-4,-7,-11]
-  False
-  *Main> isRR [-1,-4,-7,-10]
-  True
--}
+isRR (x:[y,z])  = y - x == z - y
+isRR (x:y:z:xs) = y - x == z - y && isRR (y:z:xs)
