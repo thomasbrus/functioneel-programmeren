3c2307346e8bdf767c2a64efaa25e384fa9fdbbd
Thomas Brus <thomas.brus@me.com>
2012-10-22 21:52:56 +0200

    Finished all of the last series

--- exercises-6/Graphics.hs
+++ exercises-6/Graphics.hs
@@ -4,7 +4,7 @@ import Prelude
 import FPPrac.Graphics
 import System.FilePath (splitPath, dropExtension)
 import Data.List
-
+import Data.Maybe
 
 data Thickness = Thin | Thick
   deriving (Eq,Show)
@@ -209,6 +209,12 @@ drawBottomLine graph
 -- gerichte als voor ongerichte grafen. Bij gerichte grafen mogen buren
 -- alleen in de pijlrichting worden genomen.
 
+-- *Graphics> findDirectNeighbours exampleGraph (exampleNodes !! 0)
+-- [('b',RGBA 0.0 0.0 0.0 1.0,(1.0,2.0))]
+
+-- *Graphics> findDirectNeighbours exampleGraph' (exampleNodes !! 1)
+-- [('c',RGBA 0.0 0.0 0.0 1.0,(1.0,3.0)),('d',RGBA 0.0 0.0 0.0 1.0,(1.0,4.0))]
+
 findDirectNeighbours :: Graph -> Node -> [Node]
 findDirectNeighbours g n@(c, _, _) 
   | directed g
@@ -228,11 +234,175 @@ exampleNodes =
 
 exampleEdges =
   [ ('a', 'b', black, 1)
-  , ('b', 'c', black, 1)
-  , ('b', 'd', black, 1)
+  , ('b', 'c', black, 2)
+  , ('b', 'd', black, 3)
   ]
 
 exampleGraph = Graph "exampleGraph" False False exampleNodes exampleEdges
-exampleGraph' = Graph "exampleGraph'" True False exampleNodes exampleEdges
+exampleGraph' = exampleGraph { directed = True }
 
+allEdges = 
+  [ ('a', 'b', black, 1)
+  , ('a', 'c', black, 1)
+  , ('a', 'd', black, 1)
+  , ('b', 'c', black, 1)
+  , ('b', 'd', black, 1)
+  , ('c', 'd', black, 1)
+  ]
+
+completeGraph = Graph "completeGraph" False False exampleNodes allEdges
+
+partialEdges = 
+  [ ('a', 'b', black, 1)
+  , ('c', 'd', black, 1)
+  ]
+
+disconnectedGraph = Graph "disconnectedGraph" False False exampleNodes partialEdges
+
+-- Opgave 2
+-- a. Schrijf een functie waarmee getest kan worden of een ongerichte graaf
+-- volledig is (d.w.z. er is een edge tussen elke node en elke andere node).
+-- Deze test moet expliciet alle mogelijke bindingen langslopen (dus niet
+-- alleen maar nodes en edges tellen).
+
+-- *Graphics> isComplete disconnectedGraph 
+-- False
+-- *Graphics> isComplete exampleGraph
+-- False
+-- *Graphics> isComplete completeGraph 
+-- True
+
+isComplete :: Graph -> Bool
+isComplete g = all (\n -> findDirectNeighbours g n == (nodes g) \\ [n]) (nodes g)
+
+-- b. Schrijf een functie om te testen of een ongerichte graaf
+-- samenhangend is, d.w.z. of er een pad loopt van elke node naar
+-- elke andere node. Daarbij bestaat een pad uit een aantal edges achter elkaar.
+
+-- *Graphics> isConnected exampleGraph
+-- True
+-- *Graphics> isConnected disconnectedGraph 
+-- False
+-- *Graphics> isConnected completeGraph 
+-- True
+
+isConnected :: Graph -> Bool
+isConnected g = all (\n -> intersect (nodes g \\ [n]) (reachableNodes g n) == (nodes g \\ [n])) (nodes g)
+
+removeEdge :: Graph -> Node -> Node -> Graph
+removeEdge g n1 n2 =
+  let
+    (l1, _, _) = n1
+    (l2, _, _) = n2
+    edges' = filter (\(a, b, _, _) -> (a, b) /= (l1, l2) && (a, b) /= (l2, l1)) (edges g)
+  in
+    g { edges = edges' }
+
+reachableNodes :: Graph -> Node -> [Node]
+reachableNodes g n =
+    let
+      neighbours = findDirectNeighbours g n
+    in
+      union neighbours $ concat (map (\n' -> reachableNodes (removeEdge g n n') n') neighbours)
+
+-- c. Schrijf een functie die de lijst van samenhangende subgrafen van een
+-- gegeven ongerichte graaf oplevert (met “subgrafen” worden hier de grootst
+-- mogelijke subgrafen bedoeld). Schrijf een commando waarmee die samen-
+-- hangende subgrafen elk hun eigen kleur krijgen (dwz: alle nodes uit een
+-- samenhangende subgraaf moeten dezelfde kleur krijgen. Uw functie mag zelf
+-- beslissen welke kleuren daarvoor worden gekozen).
+
+-- *Graphics> colorSubGraphs disconnectedGraph [red, blue, white, white]
+-- Graph {name = "disconnectedGraph", directed = False, weighted = False, nodes = [('a',RGBA 1.0 0.0 0.0 1.0,(1.0,1.0)),('b',RGBA 1.0 0.0 0.0 1.0,(1.0,2.0)),('c',RGBA 0.0 0.0 1.0 1.0,(1.0,3.0)),('d',RGBA 0.0 0.0 1.0 1.0,(1.0,4.0))], edges = [('a','b',RGBA 0.0 0.0 0.0 1.0,1),('c','d',RGBA 0.0 0.0 0.0 1.0,1)]}
+
+recolorNodes :: [Node] -> Color -> [Node]
+recolorNodes ns color = map (\(c, _, p) -> (c, color, p)) ns
+
+colorSubGraphs :: Graph -> [Color] -> Graph
+colorSubGraphs g cs = g { nodes = concat $ map (\(ns, c) -> recolorNodes ns c) $ zip (groupBySubGraph g) cs  }
+
+groupBySubGraph :: Graph -> [[Node]]
+groupBySubGraph g
+  | null (nodes g)
+  = []
+  | otherwise
+  = let current = (head $ nodes g); reachables = reachableNodes g current
+    in [current : reachables] ++ groupBySubGraph g { nodes = (tail $ nodes g) \\ reachables }
+
+-- Opgave 3.
+-- a. Schrijf een functie die test of in een gerichte graaf een gegeven node B
+-- bereikbaar is (in overeenstemming met de edgerichting) vanuit een node A.
+
+-- *Graphics> isReachable exampleGraph' (exampleNodes !! 1) (exampleNodes !! 0)
+-- False
+-- *Graphics> isReachable exampleGraph' (exampleNodes !! 0) (exampleNodes !! 1)
+-- True
+
+removeEdge' :: Graph -> Node -> Node -> Graph
+removeEdge' g n1 n2 =
+  let
+    (l1, _, _) = n1
+    (l2, _, _) = n2
+    edges' = filter (\(a, b, _, _) -> (a, b) /= (l1, l2)) (edges g)
+  in
+    g { edges = edges' }
+
+reachableNodes' :: Graph -> Node -> [Node]
+reachableNodes' g n =
+    let
+      neighbours = findDirectNeighbours g n
+    in
+      union neighbours $ concat (map (\n' -> reachableNodes' (removeEdge' g n n') n') neighbours)
+
+isReachable :: Graph -> Node -> Node -> Bool
+isReachable g n1 n2 = n2 `elem` (reachableNodes' g n1)
+
+-- b. Schrijf een functie die (in een gerichte graaf) de lijst van alle paden
+-- van A naar B berekent. Daarbij is verondersteld dat een pad geen lussen bevat.
+-- Definieer een commando dat één voor één die paden kleurt.
+
+-- *Graphics> findPaths exampleGraph (exampleNodes !! 0) (exampleNodes !! 1)
+-- [[('a',RGBA 0.0 0.0 0.0 1.0,(1.0,1.0)),('b',RGBA 0.0 0.0 0.0 1.0,(1.0,2.0))]]
+
+findNodeByChar :: Graph -> Char -> Maybe Node
+findNodeByChar g c = find (\(c', _, _) -> c == c') (nodes g)
+
+findPaths :: Graph -> Node -> Node -> [[Node]]
+findPaths g a b = findPaths' g a b []
+
+findPaths' :: Graph -> Node -> Node -> [Node] -> [[Node]]
+findPaths' g a@(a', _, _) b current = findPaths'' g a b current [ fromJust (findNodeByChar g y) | (x, y, _, _) <- (edges g), x == a' ]
+
+findPaths'' :: Graph -> Node -> Node -> [Node] -> [Node] -> [[Node]]
+findPaths'' g a b current []
+  | a == b    = [current ++ [b]]
+  | otherwise = []
+
+findPaths'' g a b current (x:xs)
+  | elem a current  = []
+  | otherwise       = (findPaths' g x b (current ++ [a])) ++ (findPaths'' g a b current xs)
+
+colorPaths :: Graph -> Node -> Node -> Color -> [[Node]]
+colorPaths g a b c = map (\ns -> recolorNodes ns c) (findPaths g a b)
+
+-- c. Definieer met behulp van deze functie een functie die in een 
+-- gerichte en gewogen graaf het kortste pad van A naar B vindt en kleurt.
+-- Hierbij betekent het begrip “kortst” dat het totaal van alle gewichten
+-- op het pad minimaal moet zijn.
+
+-- *Graphics> colorMinimalPath  exampleGraph' (exampleNodes !! 0) (exampleNodes !! 3) red
+-- [('a',RGBA 1.0 0.0 0.0 1.0,(1.0,1.0)),('b',RGBA 1.0 0.0 0.0 1.0,(1.0,2.0)),('d',RGBA 1.0 0.0 0.0 1.0,(1.0,4.0))]
+
+findEdgeByNodes :: Graph -> Node -> Node -> Maybe Edge
+findEdgeByNodes g (a, _, _) (b, _, _) = find (\(a', b', _, _) -> a == a' && b == b') (edges g)
+
+pathWeight :: Graph -> [Node] -> Int
+pathWeight g (n:[]) = 0
+pathWeight g (n:ns) = let (_, _, _, w) = fromJust (findEdgeByNodes g n (head ns)) in w + pathWeight g ns
+
+findMinimalPath :: Graph -> Node -> Node -> [Node]
+findMinimalPath g a b = minimumBy (\p1 p2 -> compare (pathWeight g p1) (pathWeight g p2)) (findPaths g a b)
+
+colorMinimalPath :: Graph -> Node -> Node -> Color -> [Node]
+colorMinimalPath g a b c = recolorNodes (findMinimalPath g a b) c
 
